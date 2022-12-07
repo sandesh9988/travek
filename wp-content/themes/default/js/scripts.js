859130f9a29f615a1563da6186626f5ed55b05ae
Sandesh Thapa <sandeshthapa@Apples-Mac-mini.local>
2022-12-07 16:12:24 +0545

    update : complete for first pull request

--- wp-content/themes/default/js/scripts.js
+++ wp-content/themes/default/js/scripts.js
@@ -144,9 +144,66 @@ $(function () {
 
 $(function () {
 
+    
 
+    $('.video-slide__slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        nextArrow: 'none',
+         prevArrow: 'none'
+      });
+
+      $('.image-slider__sliders').slick({
+        centerMode: true,
+        
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 300,
+        nextArrow: '.image-slider__rightarrow ',
+        prevArrow: '.image-slider__leftarrow ',
+        responsive: [
+          {
+            breakpoint: 1100,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '25px',
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '25px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+      
 
 });
+
+
+function openTab(evt, tabName) {
+  var i, tab__banner, tab__links;
+  tab__banner = document.getElementsByClassName("tab__banner");
+  for (i = 0; i < tab__banner.length; i++) {
+    tab__banner[i].style.display = "none";
+  }
+  tab__links = document.getElementsByClassName("tab__links");
+  for (i = 0; i < tab__links.length; i++) {
+    tab__links[i].className = tab__links[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "flex";
+  evt.currentTarget.className += " active";
+};
 $(function () {
 
     $('.js-header').e11_MobileNav();
